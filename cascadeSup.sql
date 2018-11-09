f43c3efbac1b07e13a1174f7ab2ca4018c69690f
CircuitSyn <circuitsyn@gmail.com>
2018-11-08 23:16:59 -0800

    sql

--- cascadeSup.sql
+++ cascadeSup.sql
@@ -3,7 +3,7 @@ USE CASCADE_DB;
 CREATE TABLE departments (
   Department_ID INT AUTO_INCREMENT,
   Department_Name VARCHAR(100) NULL,
-  Over_Head_Costs DECIMAL(10,2) NULL,
+  Over_Head_Costs DECIMAL(10,2) DEFAULT 0,
   PRIMARY KEY (Department_ID)
 );
 
@@ -12,8 +12,3 @@ VALUES ('Electronics'), ('Food'), ('Books'), ('Holiday');
 
 SELECT * FROM departments;
 
-SELECT departments.Department_ID, departments.Department_Name, departments.Over_Head_Costs, SUM(products.Sold) AS Total_Sold, SUM(products.Product_Sales) - departments.Over_Head_Costs AS Total_Profit
-FROM products
-INNER JOIN departments
-ON products.Dept_Name = departments.Department_Name
-GROUP BY departments.Department_Name;
